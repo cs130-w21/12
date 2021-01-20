55e506013c51a6e94b768a4015eb2424310f9009
Seoyoon <seoyoon.jin221@gmail.com>
2021-01-20 15:39:30 -0800

    installed babel-eslint

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -20,5 +20,6 @@ module.exports = {
   rules: {
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
     'space-before-function-paren': [0]
-  }
+  },
+  parser: 'babel-eslint'
 }
