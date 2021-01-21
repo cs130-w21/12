4a11fc35483acf10b73e725714f0ad8d2d88da49
JakinHuangWang <31380172+JakinHuangWang@users.noreply.github.com>
2021-01-21 10:49:31 +0800

    3--home page (#25)
    * Fix Input Styling and added Ingredient chip
    
    * Fix Styling bugs
    
    * Fix Enter Bug
    
    * Update Icon Size

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,11 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react'
+import '../styles/Main.css'
+// Font Awesome Imports
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// Material UI Imports
+import Chip from '@material-ui/core/Chip'
 
-const Main = () => (
-  <React.Fragment>
-    <form style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '10%' }}>
-      <input style={{ borderRadius: '5px' }} placeholder="Enter Ingredients..." />
-    </form>
-  </React.Fragment>
-)
+const Main = () => {
+  const [ingredientInput, SetIngredientInput] = useState('')
+  const [ingredients, SetIngredients] = useState(['pork', 'beef'])
+  const handleClick = () => { }
+  const handleDelete = (ingredient) => event => {
+    SetIngredients(ingredients.filter((ing) => ing !== ingredient))
+  }
+  const handleAddIngredient = () => {
+    SetIngredientInput(ingredientInput.toLowerCase())
+    if (ingredientInput !== '' && ingredients.indexOf(ingredientInput) === -1) {
+      SetIngredients([...ingredients, ingredientInput])
+      SetIngredientInput('')
+    }
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    handleAddIngredient()
+  }
+
+  return (
+    <React.Fragment>
+      <form className="mt-3" onSubmit={handleSubmit}>
+        <div className="input-group">
+          <input type="text" value={`${ingredientInput}`} onChange={(event) => { SetIngredientInput(event.target.value) }} className="ingredient-input" placeholder="Enter Ingredients..." />
+          <button type="button" onClick={handleAddIngredient} className="submit-btn">
+            <FontAwesomeIcon icon={faSearch} size="lg" />
+          </button>
+        </div>
+      </form>
+
+      <div className="chip-area mt-4">
+        {
+          ingredients.map(ingredient =>
+            <Chip
+              className="mx-2 my-2 capitalize"
+              variant="outlined"
+              key={ingredient}
+              label={`${ingredient}`}
+              onClick={handleClick}
+              style={{ border: '1px solid rgba(235, 73, 23, 0.72)' }}
+              onDelete={handleDelete(ingredient)}
+            />
+          )
+        }
+      </div>
+    </React.Fragment>
+  )
+}
 
 export default Main
