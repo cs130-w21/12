24d4c01bae02f5a3e3afaef5b660675763732fc2
Jakin Wang <jakinw0508@g.ucla.edu>
2021-01-20 16:43:07 -0800

    Fix Styling bugs

--- frontend/src/components/AppHeader.js
+++ frontend/src/components/AppHeader.js
@@ -9,7 +9,7 @@ import LoginButton from './LoginButton'
 const AppHeader = () => (
   <AppBar position='static' style={{ background: '#FFFFFF' }}>
     <Toolbar>
-      <Typography variant='h4' color='inherit' style={{ fontFamily: 'Roboto Condensed' }}>
+      <Typography variant='h4' color='inherit'>
         Cuisine Machine
       </Typography>
       <div style={{ flex: 1 }} />
