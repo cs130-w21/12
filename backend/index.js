6d7bc8b3e65127dea7ec92cc65134edbd74c1680
Jason Lai <44538145+jasonlaiys@users.noreply.github.com>
2021-01-23 11:50:25 +0800

    Setup index.js (#31)

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const express = require('express');
+const cors = require('cors');
+
+const ingredients = require('./ingredients.json');
+
+const app = express();
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.json(ingredients);
+});
+
+const port = 8080;
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`); // eslint-disable-line no-console
+});
