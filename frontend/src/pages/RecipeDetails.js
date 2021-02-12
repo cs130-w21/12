df8145262c9bd55b136aeb39347b7d780e6cf219
Mingchao Lian <lianmccc@gmail.com>
2021-02-11 17:45:30 -0800

    layout

--- frontend/src/pages/RecipeDetails.js
+++ frontend/src/pages/RecipeDetails.js
@@ -1,24 +1,44 @@
 import React from 'react'
-// import { makeStyles } from '@material-ui/core/styles'
-import {
-  Grid,
-  Link,
-  Paper
-} from '@material-ui/core/'
+import { makeStyles } from '@material-ui/core/styles'
+import ScheduleIcon from '@material-ui/icons/Schedule'
+import KitchenIcon from '@material-ui/icons/Kitchen'
+import FastfoodIcon from '@material-ui/icons/Fastfood'
+// import Typography from '@material-ui/core/Typography'
+import Grid from '@material-ui/core/Grid'
+import Link from '@material-ui/core/Link'
+import Paper from '@material-ui/core/Paper'
 
-// const useStyles = makeStyles(theme => ({
-//   root: {
-//     flexGrow: 1,
-//     padding: theme.spacing(2)
-//   },
-//   grid: {
-//     width: '80%',
-//     margin: '0px'
-//   }
-// }))
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+    padding: theme.spacing(2)
+  },
+  grid: {
+    width: '70%',
+    margin: '10px'
+  }
+}))
 
 const RecipeDetails = () => {
-//   const classes = useStyles()
+  const classes = useStyles()
+
+  function FormRow() {
+    return (
+      <React.Fragment>
+        <Grid item xs={8}>
+          <Paper className={classes.paper}>
+            <h3>Pasta with Garlic, Scallions, Cauliflower & Breadcrumbs</h3>
+            <p><ScheduleIcon/> ready in 45 mins</p>
+          </Paper>
+        </Grid>
+        <Grid item xs={4}>
+          <Paper className={classes.paper}>
+            <img src="https://spoonacular.com/recipeImages/716429-556x370.jpg"/>
+          </Paper>
+        </Grid>
+      </React.Fragment>
+    )
+  }
 
   return (
     <React.Fragment>
@@ -26,11 +46,37 @@ const RecipeDetails = () => {
         back to recommended recipes
       </Link>
 
-      <Grid containers spacing={2}>
-        <Grid item>
-          <Paper>
-            testtest
-          </Paper>
+      <Grid container spacing={1}>
+        <Grid container item spacing={3}>
+          <FormRow />
+        </Grid>
+
+        <Grid container item spacing={3} xs={10}>
+          <h4><KitchenIcon/> Ingredients</h4>
+          <p> Ingredients Ingredients Ingredients
+          Ingredients Ingredients Ingredients
+          Ingredients Ingredients Ingredients
+          Ingredients Ingredients Ingredients
+          </p>
+        </Grid>
+
+        <Grid container item spacing={3} xs={10}>
+          <h4><FastfoodIcon/>Instructions</h4>
+          <p> InstructionsInstructions InstructionsInstructionsInstructions Instructions Instructions
+          InstructionsInstructions InstructionsInstructionsInstructions Instructions Instructions
+          InstructionsInstructions InstructionsInstructionsInstructions Instructions Instructions
+          InstructionsInstructions InstructionsInstructionsInstructions Instructions Instructions
+          InstructionsInstructions InstructionsInstructionsInstructions Instructions Instructions
+          InstructionsInstructions InstructionsInstructionsInstructions Instructions Instructions
+          InstructionsInstructions InstructionsInstructionsInstructions Instructions Instructions
+          InstructionsInstructions InstructionsInstructionsInstructions Instructions Instructions
+          InstructionsInstructions InstructionsInstructionsInstructions Instructions Instructions
+          InstructionsInstructions InstructionsInstructionsInstructions Instructions Instructions
+          InstructionsInstructions InstructionsInstructionsInstructions Instructions Instructions
+          InstructionsInstructions InstructionsInstructionsInstructions Instructions Instructions
+          InstructionsInstructions InstructionsInstructionsInstructions Instructions Instructions
+          InstructionsInstructions InstructionsInstructionsInstructions Instructions Instructions
+          </p>
         </Grid>
       </Grid>
     </React.Fragment>
