6cbfdd0ebf459a449490186c17a1962b3f9e0deb
Mingchao Lian <lianmccc@gmail.com>
2021-02-12 11:58:28 -0800

    basic UI design

--- frontend/src/pages/RecipeDetails.js
+++ frontend/src/pages/RecipeDetails.js
@@ -6,7 +6,7 @@ import ScheduleIcon from '@material-ui/icons/Schedule'
 import KitchenIcon from '@material-ui/icons/Kitchen'
 import FastfoodIcon from '@material-ui/icons/Fastfood'
 import PeopleIcon from '@material-ui/icons/People'
-// import BookmarkIcon from '@material-ui/icons/Bookmark';
+import BookmarkIcon from '@material-ui/icons/Bookmark'
 import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder'
 import ArrowBackIcon from '@material-ui/icons/ArrowBack'
 import CheckIcon from '@material-ui/icons/Check'
@@ -31,6 +31,8 @@ const GetIngredients = () => {
       <p> 1 Tbsp butter </p>
       <p> 2 cups cauliflower florets </p>
       <p> 2 tbsp grated cheese </p>
+      <p>1 Tbsp extra virgin olive oil</p>
+      <p>5 cloves garlic</p>
       </Typography>
     </React.Fragment>
   )
@@ -39,7 +41,16 @@ const GetIngredients = () => {
 const GetInstruction = () => {
   return (
     <React.Fragment>
-      <p> Instruction Instruction Instruction </p>
+      <h5>Step 1</h5>
+      <p>
+        Put the garlic in a pan and then add the onion.
+      </p>
+
+      <h5>Step 2</h5>
+      <p>
+        Add some salt and oregano.
+      </p>
+
     </React.Fragment>
   )
 }
@@ -90,24 +101,35 @@ const CheckVegetarian = (vegetarian) => {
 const RecipeDetails = () => {
   const classes = useStyles()
 
+  const [bookmarked, setBookmarked] = React.useState(false)
+  const handleBookmarkClick = () => {
+    setBookmarked(!bookmarked)
+  }
+
   return (
     <React.Fragment>
 
       <Grid container>
         <Grid item xs={2}></Grid>
-        <Grid item xs={10} container direction="column" spacing={3}>
+        <Grid item xs={10} container direction="column" spacing={4}>
+          <Grid item xs={10}></Grid>
           <Grid item xs={10}>
             <Link>  <ArrowBackIcon/> back to recommended recipes </Link>
           </Grid>
 
           <Grid item container direction="row">
             <Grid item xs={6}>
-              <h3 className={classes.root}>
+              <h2 className={classes.root}>
                 Pasta with Garlic, Scallions, Cauliflower & Breadcrumbs
-                <IconButton aria-label="add to favorites" className={classes.root}>
-                  <BookmarkBorderIcon/>
+                <IconButton
+                  aria-label="add to favorites"
+                  className={classes.root}
+                  onClick={handleBookmarkClick}
+                >
+                  {bookmarked && <BookmarkIcon />}
+                  {!bookmarked && <BookmarkBorderIcon />}
                 </IconButton>
-              </h3>
+              </h2>
               <p> <ScheduleIcon/> Ready in 45 mins  <PeopleIcon/> Servings: 4</p>
               <p> <CheckGlutenFree true/> </p>
               <p> <CheckVegan false/> </p>
@@ -121,12 +143,12 @@ const RecipeDetails = () => {
           </Grid>
 
           <Grid item xs={8}>
-            <h4 className={classes.root}> <KitchenIcon/> Ingredients </h4>
+            <h3 className={classes.root}> <KitchenIcon/> Ingredients </h3>
             <GetIngredients />
           </Grid>
 
           <Grid item xs={8}>
-            <h4 className={classes.root}> <FastfoodIcon/> Instructions </h4>
+            <h3 className={classes.root}> <FastfoodIcon/> Instructions </h3>
             <GetInstruction />
             <Link target="_blank" href='http://fullbellysisters.blogspot.com/2012/06/pasta-with-garlic-scallions-cauliflower.html'>
               Read the detailed instructions
@@ -134,7 +156,7 @@ const RecipeDetails = () => {
           </Grid>
 
           <Grid item xs={8}>
-            <h4 className={classes.root}> <LocalPharmacyIcon/> Nutrition Facts </h4>
+            <h3 className={classes.root}> <LocalPharmacyIcon/> Nutrition Facts </h3>
             <GetNutritionFacts />
           </Grid>
 
