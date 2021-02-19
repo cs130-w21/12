935b97bfab5c18a55ec6aea7b8d6459f20af7be3
Jason Lai <jasonyslai1@gmail.com>
2021-02-18 22:08:13 -0800

    Clean up

--- backend/route/search.js
+++ backend/route/search.js
@@ -7,6 +7,20 @@ const service = require('../service/recipe-service.js');
 const router = express.Router();
 
 /*
+GET /recipes
+This endpoint is used to get random recipes
+*/
+router.get('/', async (req, res) => {
+  try {
+    const recipes = await service.getRandomRecipes();
+    res.status(200).json({ recipes });
+  } catch (err) {
+    console.log(err); // eslint-disable-line no-console
+    res.status(500).json({ message: 'Server error' });
+  }
+});
+
+/*
 POST /recipes
 This endpoint is used to search for recipes by ingredients and preferences
 */
@@ -33,20 +47,6 @@ router.get('/:id', async (req, res) => {
   try {
     const recipeInfo = await service.getRecipeInfo(req.params.id);
     res.status(200).json({ recipeInfo });
-  } catch (err) {
-    console.log(err); // eslint-disable-line no-console
-    res.status(500).json({ message: 'Server error' });
-  }
-});
-
-/*
-GET /recipes
-This endpoint is used to get random recipes
-*/
-router.get('/', async (req, res) => {
-  try {
-    const recipes = await service.getRandomRecipes();
-    res.status(200).json({ recipes });
   } catch (err) {
     console.log(err); // eslint-disable-line no-console
     res.status(500).json({ message: 'Server error' });
