5de5693623b3f8b1e4a65380133b9c668a74cef0
Jakin Wang <jakinw0508@g.ucla.edu>
2021-02-28 23:47:37 -0800

    Fix ESLint Error

--- frontend/src/__test__/RecipeCollection.test.js
+++ frontend/src/__test__/RecipeCollection.test.js
@@ -3,6 +3,16 @@ import { MemoryRouter } from 'react-router-dom'
 import { render } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import RecipeCollection from '../pages/RecipeCollection'
+import "@okta/okta-react";
+
+jest.mock('@okta/okta-react', () => ({
+    useOktaAuth: () => {
+      return {
+        authState: {},
+        authService: {}
+    };
+  }
+}))
 
 test('check page element', async () => {
     const { getByText } = render(<RecipeCollection />, { wrapper: MemoryRouter })
