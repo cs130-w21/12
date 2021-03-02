626a2c521a4732cf3e203bfcc8e0697cf758d214
Mingchao Lian <lianmccc@gmail.com>
2021-03-02 00:19:13 -0800

    service test setup testing

---
+++ backend/__test__/database.test.js
@@ -0,0 +1,5 @@
+'use strict';
+
+// const bookmarkQuery = require('../service/bookmark-query.js');
+// const recipeQuery = require('../service/recipe-query.js');
+// const userQuery = require('../service/user-query.js');
---
+++ backend/__test__/service.test.js
@@ -0,0 +1,5 @@
+'use strict';
+
+// const bookmarkService = require('../service/bookmark-service.js');
+// const profileService = require('../service/profile-service.js');
+// const recipeService = require('../service/recipe-service.js');
