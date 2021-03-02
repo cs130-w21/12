d218c3d0be733ce3534841d14ec865c48845f98d
Jason Lai <jasonyslai1@gmail.com>
2021-03-01 17:43:52 -0800

    Added JSDoc documentation for database queries

--- backend/postgres/recipe-query.js
+++ backend/postgres/recipe-query.js
@@ -1,13 +1,37 @@
+/**
+ * @module postgres/recipe-query
+ * @description This module encapsulates database access to the Recipe table.
+ * @requires module:model/database
+ * @requires module:model/Recipe
+ */
+
 'use strict';
 
 const { sequelize } = require('../model/database.js');
 const Recipe = require('../model/Recipe.js');
 
+/**
+ * @async
+ * @function getRecipeByRecipeId
+ * @param {int} recipeId - Recipe ID
+ * @returns {Promise} A Promise representing the result of the SQL transaction.
+ * @description This method gets exactly zero or one Recipe object from the Recipe table given the
+ * recipe ID.
+ */
 const getRecipeByRecipeId = (recipeId) => sequelize.transaction((t) => Recipe.findOne({
   where: { id: recipeId },
   transaction: t,
 }));
 
+/**
+ * @async
+ * @function ensureRecipe
+ * @param {Recipe} r - A recipe object
+ * @returns {Promise} A Promise representing the result of the SQL transaction.
+ * @description This method checks if the given Recipe object exists in the Recipe table. If it does
+ * not exist, it will insert a new row into the Recipe table that corresponds to the given Recipe
+ * object.
+ */
 const ensureRecipe = (r) => sequelize.transaction((t) => Recipe.findOrCreate({
   where: { id: r.id },
   defaults: { title: r.title, image_url: r.imageUrl },
--- backend/postgres/user-query.js
+++ backend/postgres/user-query.js
@@ -1,6 +1,8 @@
 /**
- * This module encapsulates access to the User table.
  * @module postgres/user-query
+ * @description This module encapsulates database access to the User table.
+ * @requires module:model/database
+ * @requires module:model/User
  */
 
 'use strict';
@@ -11,7 +13,10 @@ const User = require('../model/User.js');
 /**
  * @async
  * @function getUserInfo
- * @param {String} userId - User ID
+ * @param {UUID} userId - User ID
+ * @returns {Promise} A Promise representing the result of the SQL transaction.
+ * @description This method gets exactly zero or one User object from the User table given the user
+ * ID.
  */
 const getUserInfo = (userId) => sequelize.transaction((t) => User.findOne({
   where: { uuid: userId },
@@ -21,7 +26,10 @@ const getUserInfo = (userId) => sequelize.transaction((t) => User.findOne({
 /**
  * @async
  * @function ensureUser
- * @param {String} userId - User ID
+ * @param {UUID} userId - User ID
+ * @returns {Promise} A Promise representing the result of the SQL transaction.
+ * @description This method checks if the given user ID exists in the User table. If it does not
+ * exist, it will insert a new row into the User table that corresponds to a User object.
  */
 const ensureUser = (userId) => sequelize.transaction((t) => User.findOrCreate({
   where: { uuid: userId },
