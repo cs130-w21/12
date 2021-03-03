b3e2d6242286f5fad4acae99135bacb179a9e309
Jason Lai <jasonyslai1@gmail.com>
2021-03-02 23:16:07 -0800

    Restructure backend package structure

--- backend/src/util/logger.js
+++ backend/src/util/logger.js
@@ -2,7 +2,7 @@
 
 const fs = require('fs');
 
-const config = require('../config.js');
+const config = require('../../config.js');
 
 const logStream = fs.createWriteStream(config.logFile, { flags: 'a' });
 const log = (str) => logStream.write(str);
