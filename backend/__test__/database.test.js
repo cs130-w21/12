7e484e6521a898b54491becf96616ae6ad957bf5
Sim <sdhaliwal@ucla.edu>
2021-03-02 23:01:02 -0800

    Progress :(

--- backend/__test__/database.test.js
+++ backend/__test__/database.test.js
@@ -1,7 +1,29 @@
+/* eslint-disable global-require */
 /* eslint-disable no-undef */
 
 'use strict';
 
-// const bookmarkQuery = require('../postgres/user-query.js');
+const userQuery = require('../postgres/user-query.js');
 // const recipeQuery = require('../service/recipe-query.js');
 // const userQuery = require('../service/user-query.js');
+jest.mock('../model/database.js', () => {
+  const SequelizeMock = require('sequelize-mock');
+  return { sequelize: new SequelizeMock() };
+});
+
+jest.mock('../model/User.js', () => () => {
+  const SequelizeMock = require('sequelize-mock');
+  const dbMock = new SequelizeMock();
+  return dbMock.define('User', {
+    uuid: '9f50a9ff-273b-42df-8438-9e5adb6c675e',
+  }, {
+    
+  });
+});
+
+describe('Test Sequelize Mocking', () => {
+  it('Should get value from mock', async () => {
+    const user = await userQuery.getUserInfo('9f50a9ff-273b-42df-8438-9e5adb6c675e');
+    expect(user.uuid).toEqual('9f50a9ff-273b-42df-8438-9e5adb6c675e');
+  });
+});
--- backend/__test__/service.test.js
+++ backend/__test__/service.test.js
@@ -1,17 +1,22 @@
+/* eslint-disable max-len */
 /* eslint-disable no-undef */
 
 'use strict';
 
-const { ensureUser } = require('../postgres/user-query');
+// const bookmarkQuery = require('../postgres/bookmark-query');
+// const syncAllTables = require('../model/sync.js');
+// // const { ensureUser } = require('../postgres/user-query');
 
-const bookmarkService = jest.requireMock('../service/bookmark-service.js');
-jest.mock('../postgres/user-query.js', () => ({
-  __esModule: true,
-  ensureUser: jest.fn(() => 'p'),
-}));
+// const bookmarkService = jest.requireMock('../service/bookmark-service.js');
+// jest.mock('../postgres/user-query.js', () => ({
+//   __esModule: true,
+//   ensureUser: jest.fn(() => 'p'),
+// }));
 
-// const bookmark = jest.requireMock('../model/Bookmark.js');
+// // const bookmark = jest.requireMock('../model/Bookmark.js');
 
-test('the data is peanut butter', async () => {
-  await expect(bookmarkService.getBookmarks('9f50a9ff-273b-42df-8438-9e5adb6c675e')).toBe(expect.anything());
-});
+// beforeAll(() => syncAllTables());
+
+// test('the data is peanut butter', async () => {
+//   await expect(bookmarkService.getBookmarks('9f50a9ff-273b-42df-8438-9e5adb6c675e')).toBe(expect.anything());
+// });
