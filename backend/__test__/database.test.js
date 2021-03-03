40a4953f7df6263bfbe5b574bb6d25813836a25d
Sim <sdhaliwal@ucla.edu>
2021-03-02 21:02:11 -0800

    Testing tests

--- backend/__test__/database.test.js
+++ backend/__test__/database.test.js
@@ -1,5 +1,7 @@
+/* eslint-disable no-undef */
+
 'use strict';
 
-// const bookmarkQuery = require('../service/bookmark-query.js');
+// const bookmarkQuery = require('../postgres/user-query.js');
 // const recipeQuery = require('../service/recipe-query.js');
 // const userQuery = require('../service/user-query.js');
--- backend/__test__/service.test.js
+++ backend/__test__/service.test.js
@@ -1,5 +1,17 @@
+/* eslint-disable no-undef */
+
 'use strict';
 
-// const bookmarkService = require('../service/bookmark-service.js');
-// const profileService = require('../service/profile-service.js');
-// const recipeService = require('../service/recipe-service.js');
+const { ensureUser } = require('../postgres/user-query');
+
+const bookmarkService = jest.requireMock('../service/bookmark-service.js');
+jest.mock('../postgres/user-query.js', () => ({
+  __esModule: true,
+  ensureUser: jest.fn(() => 'p'),
+}));
+
+// const bookmark = jest.requireMock('../model/Bookmark.js');
+
+test('the data is peanut butter', async () => {
+  await expect(bookmarkService.getBookmarks('9f50a9ff-273b-42df-8438-9e5adb6c675e')).toBe(expect.anything());
+});
