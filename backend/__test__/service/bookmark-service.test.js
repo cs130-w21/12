562594d3d72dcc406ecf861dcfc57d9500e39d83
Jason Lai <jasonyslai1@gmail.com>
2021-03-03 22:16:06 -0800

    Completed tests for bookmark-service

--- backend/__test__/service/bookmark-service.test.js
+++ backend/__test__/service/bookmark-service.test.js
@@ -3,20 +3,50 @@
 
 'use strict';
 
-// const bookmarkQuery = require('../postgres/bookmark-query');
-// const syncAllTables = require('../model/sync.js');
-// // const { ensureUser } = require('../postgres/user-query');
+const bookmarkQuery = require('../../src/postgres/bookmark-query.js');
+const bookmarkService = require('../../src/service/bookmark-service.js');
+const recipeQuery = require('../../src/postgres/recipe-query.js');
+const userQuery = require('../../src/postgres/user-query.js');
 
-// const bookmarkService = jest.requireMock('../service/bookmark-service.js');
-// jest.mock('../postgres/user-query.js', () => ({
-//   __esModule: true,
-//   ensureUser: jest.fn(() => 'p'),
-// }));
+describe('Test get all user bookmarks app service', () => {
+  it('Should succeed', async () => {
+    userQuery.ensureUser = jest.fn(() => Promise.resolve());
+    bookmarkQuery.getBookmarks = jest.fn(() => [{
+      id: 1,
+      userId: '9f50a9ff-273b-42df-8438-9e5adb6c675e',
+      recipeId: 640136,
+    }]);
+    recipeQuery.getRecipeByRecipeId = jest.fn(() => ({
+      id: 640136,
+      title: 'Corned Beef And Cabbage With Irish Mustard Sauce',
+      imageUrl: 'https://spoonacular.com/recipeImages/640136-556x370.jpg',
+    }));
 
-// // const bookmark = jest.requireMock('../model/Bookmark.js');
+    await bookmarkService.getBookmarks('9f50a9ff-273b-42df-8438-9e5adb6c675e');
+    expect(userQuery.ensureUser).toBeCalledTimes(1);
+    expect(bookmarkQuery.getBookmarks).toBeCalledTimes(1);
+    expect(recipeQuery.getRecipeByRecipeId).toBeCalledTimes(1);
+  });
+});
 
-// beforeAll(() => syncAllTables());
+describe('Test adding user bookmark', () => {
+  it('Should succeed', async () => {
+    userQuery.ensureUser = jest.fn(() => Promise.resolve());
+    bookmarkQuery.addBookmark = jest.fn(() => Promise.resolve());
 
-// test('the data is peanut butter', async () => {
-//   await expect(bookmarkService.getBookmarks('9f50a9ff-273b-42df-8438-9e5adb6c675e')).toBe(expect.anything());
-// });
+    await bookmarkService.addBookmark('9f50a9ff-273b-42df-8438-9e5adb6c675e', 640136);
+    expect(userQuery.ensureUser).toBeCalledTimes(1);
+    expect(bookmarkQuery.addBookmark).toBeCalledTimes(1);
+  });
+});
+
+describe('Test deleting user bookmark', () => {
+  it('Should succeed', async () => {
+    userQuery.ensureUser = jest.fn(() => Promise.resolve());
+    bookmarkQuery.deleteBookmark = jest.fn(() => Promise.resolve());
+
+    await bookmarkService.deleteBookmark('9f50a9ff-273b-42df-8438-9e5adb6c675e', 640136);
+    expect(userQuery.ensureUser).toBeCalledTimes(1);
+    expect(bookmarkQuery.deleteBookmark).toBeCalledTimes(1);
+  });
+});
